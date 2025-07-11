8ea8de394e01b479d770bad1059ba10d7713b41e
Ngakan Gandhi <ngandhi@pmintl.net>
2025-07-11 11:15:35 +0700

    first commit

---
+++ weeks/week-01/project-starter.sql
@@ -0,0 +1,6 @@
+-- Week 1 Project: Personal Library Database
+-- Complete the following tasks to create your personal library system
+
+-- Task 1: Create the database
+CREATE DATABASE personal_library;
+
